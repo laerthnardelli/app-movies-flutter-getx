4801a716066cbd6deb9f84fd1505d9d0b0098d2d
laerthnardelli <laerthnardelli@gmail.com>
2021-09-13 14:11:33 -0300

    added assets  images / config - and SplashPage

--- movies/lib/modules/splash/splash_page.dart
+++ movies/lib/modules/splash/splash_page.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_remote_config/firebase_remote_config.dart';
+//import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -7,12 +8,17 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
       body: Center(
         child: Container(
-          child: Text(RemoteConfig.instance.getString('api_token')),
+          width: Get.width, //width: MediaQuery.of(context).size.width,
+          height: Get.height, //height: MediaQuery.of(context).size.height,
+          //child: Text(RemoteConfig.instance.getString('api_token')),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover),
+          ),
+          child: Image.asset('assets/images/logo.png'),
         ),
       ),
     );
