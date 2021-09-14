a5778aa3fceba4a18f09887bc934aa25da9fa92f
laerthnardelli <laerthnardelli@gmail.com>
2021-09-14 10:38:31 -0300

    added layout Login Page

--- movies/lib/modules/login/login_page.dart
+++ movies/lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,11 +8,44 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: Container(
-        child: const Text('Login Page'),
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Image.asset(
+            'assets/images/background.png',
+            width: Get.width,
+            height: Get.height,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: Colors.black45,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  width: Get.width *
+                      .9, //width: MediaQuery.of(context).size.width *.9,
+                  height: 42,
+                  child: SignInButton(
+                    Buttons.Google,
+                    text: 'Entrar com o Google',
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100)),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
