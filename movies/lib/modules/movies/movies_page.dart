01358bcb8fc7dd06c33501460a05bc52fd62de79
laerthnardelli <laerthnardelli@gmail.com>
2021-09-16 20:25:06 -0300

    component movies header

--- movies/lib/modules/movies/movies_page.dart
+++ movies/lib/modules/movies/movies_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:movies/modules/movies/widgets/movies_header.dart';
 
 class MoviesPage extends StatelessWidget {
   const MoviesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: const Text('Movies Page'),
+       return SizedBox(
+      width: Get.width,
+      child: ListView(
+        children: [
+          MoviesHeader(),
+        ],
       ),
     );
   }
