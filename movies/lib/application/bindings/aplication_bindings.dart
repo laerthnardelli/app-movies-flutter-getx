f0b2e73a4d6cbd686611d56f7b5a4bdc4006eb94
laerthnardelli <laerthnardelli@gmail.com>
2021-09-19 17:40:35 -0300

    added MoviesRepository and MoviesServie  in ApplicationBindings

--- movies/lib/application/bindings/aplication_bindings.dart
+++ movies/lib/application/bindings/aplication_bindings.dart
@@ -3,19 +3,29 @@ import 'package:movies/application/auth/auth_service.dart';
 import 'package:movies/application/rest_client/rest_client.dart';
 import 'package:movies/repositories/login/login_repository.dart';
 import 'package:movies/repositories/login/login_repository_impl.dart';
+import 'package:movies/repositories/movies/movies_repository.dart';
+import 'package:movies/repositories/movies/movies_repository_impl.dart';
 import 'package:movies/services/login/login_service.dart';
 import 'package:movies/services/login/login_service_impl.dart';
+import 'package:movies/services/movies/movies_service.dart';
+import 'package:movies/services/movies/movies_service_impl.dart';
 
 class ApplicationBindings implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => RestClient());
-    Get.lazyPut<LoginRepository>(() => LoginRepositoryImpl(), fenix: true);
+    Get.lazyPut<LoginRepository>(
+      () => LoginRepositoryImpl(),
+      fenix: true,
+    );
     Get.lazyPut<LoginService>(
         () => LoginServiceImpl(loginRepository: Get.find()),
         fenix: true);
     Get.put(AuthService()).init();
+
+    Get.lazyPut<MoviesRepository>(() => MoviesRepositoryImpl(restClient: Get.find()));
+    Get.lazyPut<MoviesServie>(() => MoviesServiceImpl(moviesRepository: Get.find()));
   }
 }
 
-class MoviesRepositoryImpl {}
+
