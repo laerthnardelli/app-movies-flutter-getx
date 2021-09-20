59583b565a7422c9633eab5827ae6019ea52764e
laerthnardelli <laerthnardelli@gmail.com>
2021-09-20 16:13:17 -0300

    added MovieDetailContentCredits

--- movies/lib/modules/movie_detail/widgets/movie_detail_content/movie_detail_content.dart
+++ movies/lib/modules/movie_detail/widgets/movie_detail_content/movie_detail_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies/models/movie_detail_model.dart';
 import 'package:movies/modules/movie_detail/widgets/movie_detail_content/movie_detail_content_title.dart';
+import 'package:movies/modules/movie_detail/widgets/movie_detail_content_credits.dart';
 
 class MovieDetailContent extends StatelessWidget {
   final MovieDetailModel? movie;
@@ -12,6 +13,7 @@ class MovieDetailContent extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         MovieDetailContentTitle(movie: movie),
+        MovieDetailContentCredits(movie: movie),
       ],
     );
   }
---
+++ movies/lib/modules/movie_detail/widgets/movie_detail_content_credits.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:movies/models/movie_detail_model.dart';
+
+class MovieDetailContentCredits extends StatelessWidget {
+  final MovieDetailModel? movie;
+  const MovieDetailContentCredits({Key? key, required this.movie})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(minHeight: 50),
+        child: Text(
+          movie?.overview ?? '',
+          style: TextStyle(fontSize: 14, height: 1.3),
+        ),
+      ),
+    );
+  }
+}
