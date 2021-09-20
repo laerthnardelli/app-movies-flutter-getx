4727b79b18fff0dfd99c90938737adfa37d12c54
laerthnardelli <laerthnardelli@gmail.com>
2021-09-20 15:16:08 -0300

    logic implementation of MoveiDetailController and MovieDetailBindings

--- movies/lib/modules/movie_detail/movie_detail_bindings.dart
+++ movies/lib/modules/movie_detail/movie_detail_bindings.dart
@@ -4,6 +4,6 @@ import 'package:movies/modules/movie_detail/movie_detail_controller.dart';
 class MovieDetailBindings implements Bindings {
   @override
   void dependencies() {
-    Get.put(MoveiDetailController());
+    Get.put(MoveiDetailController(moviesService: Get.find()));
   }
 }
--- movies/lib/modules/movie_detail/movie_detail_controller.dart
+++ movies/lib/modules/movie_detail/movie_detail_controller.dart
@@ -1,3 +1,44 @@
 import 'package:get/get.dart';
 
-class MoveiDetailController extends GetxController {}
+import 'package:movies/application/ui/loader/loader_mixin.dart';
+import 'package:movies/application/ui/messages/messages_mixin.dart';
+import 'package:movies/models/movie_detail_model.dart';
+import 'package:movies/services/movies/movies_service.dart';
+
+class MoveiDetailController extends GetxController
+    with LoaderMixin, MaessagesMixin {
+  final MoviesService _moviesService;
+
+  var loading = false.obs;
+  var message = Rxn<MessageModel>();
+  var movie = Rxn<MovieDetailModel>();
+
+  MoveiDetailController({required MoviesService moviesService})
+      : _moviesService = moviesService;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loaderListener(loading);
+    messageListener(message);
+  }
+
+  @override
+  void onReady() async {
+    super.onReady();
+    try {
+      final movieId = Get.arguments;
+      loading(true);
+      final movieDetailData = await _moviesService.getDetail(movieId);
+      movie.value = movieDetailData;
+      movie(movieDetailData);
+      loading(false);
+    } catch (e, s) {
+      print(e);
+      print(s);
+      loading(false);
+      message(MessageModel.error(
+          title: 'Erro', message: 'Erro ao buscar detalhes do filme'));
+    }
+  }
+}
